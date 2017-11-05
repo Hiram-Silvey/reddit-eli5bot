1f9188fdf218baef8545d2d9cf56cc305cd365c6
Brian Silvey <blujay270@gmail.com>
2017-11-04 21:03:27 -0400

    updated bot to call simpli5 functions

--- eli5bot.py
+++ eli5bot.py
@@ -1,17 +1,13 @@
-import simpli5
+#import simpli5
 import praw
 import re
 
 reddit = praw.Reddit('eli5bot')
-
 subreddit = reddit.subreddit("all")
 
 for comment in subreddit.stream.comments():
-    print(comment.body)
-    if re.search("!eli5", comment.body, re.IGNORECASE):
-            # get parent comment and run it through SMMRY -> simpli5
-            # comment.reply(the result)
-            # print(the result)
-            
-
-
+    if re.search("eli5", comment.body, re.IGNORECASE):
+        parent_comment = comment.parent.body
+        transformed = simpli5.parser.simpli5(simpli5.worker.smmrize(parent_comment))
+        comment.reply(transformed)
+        print(transformed)
