b1b3d8b31fd6fb2a7c7f473370f76115ac702615
Brian Silvey <blujay270@gmail.com>
2017-11-05 04:02:43 -0500

    added prints and updated simpli5

--- eli5bot.py
+++ eli5bot.py
@@ -5,8 +5,10 @@ import re
 reddit = praw.Reddit('eli5bot')
 subreddit = reddit.subreddit("all")
 
+SIGNATURE = 'beep boop. I am the eli5bot. I attempt to translate things to simpler english and link relevant wiki articles.'
+
 for comment in subreddit.stream.comments():
-    if re.search("i", comment.body, re.IGNORECASE):
+    if re.search("e", comment.body, re.IGNORECASE):
         parent = comment.parent()
         if isinstance(parent, praw.models.Submission):
             continue
@@ -17,3 +19,5 @@ for comment in subreddit.stream.comments():
         #comment.reply(transformed)
         print '\nafter:'
         print transformed
+        result = '> ' + transformed + '\n*****\nI am eli5bot. I attempt to simplify text and provide relevant wiki links. Beep boop.'
+        break
