0137f052255f734fd57afed72983d6eff78ce8c4
yuhu0621 <hu28y@mtholyoke.edu>
2017-11-05 05:42:53 -0500

    Add a timer

--- eli5bot.py
+++ eli5bot.py
@@ -1,16 +1,22 @@
 import simpli5.parser as smpl
 import praw
 import re
-
+import time
 reddit = praw.Reddit('eli5bot')
-subreddit = reddit.subreddit("all")
+subreddit = reddit.subreddit("science")
 
 for comment in subreddit.stream.comments():
-    if re.search("i", comment.body, re.IGNORECASE):
+    if re.search("eli5", comment.body, re.IGNORECASE):
         parent = comment.parent()
         if isinstance(parent, praw.models.Submission):
             continue
         parent_comment = comment.parent().body
         transformed = smpl.simpli5(smpl.smmrize(parent_comment))
-        #comment.reply(transformed)
+        try:
+            comment.reply(transformed)
+
+        except:
+            print "Need to wait for at least 10 minutes to make another comment"
+            time.sleep(600)
+            comment.reply(transformed)
         print(transformed)
