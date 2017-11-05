aac6fb7c45d408a95b5165c5413e813e418fe168
Brian Silvey <blujay270@gmail.com>
2017-11-04 23:04:15 -0400

    added nested dir and updated bot

--- eli5bot.py
+++ eli5bot.py
@@ -1,4 +1,4 @@
-#import simpli5
+import simpli5.parser as smpl
 import praw
 import re
 
@@ -6,8 +6,11 @@ reddit = praw.Reddit('eli5bot')
 subreddit = reddit.subreddit("all")
 
 for comment in subreddit.stream.comments():
-    if re.search("eli5", comment.body, re.IGNORECASE):
-        parent_comment = comment.parent.body
-        transformed = simpli5.parser.simpli5(simpli5.worker.smmrize(parent_comment))
-        comment.reply(transformed)
+    if re.search("i", comment.body, re.IGNORECASE):
+        parent = comment.parent()
+        if isinstance(parent, praw.models.Submission):
+            continue
+        parent_comment = comment.parent().body
+        transformed = smpl.simpli5(smpl.smmrize(parent_comment))
+        #comment.reply(transformed)
         print(transformed)
